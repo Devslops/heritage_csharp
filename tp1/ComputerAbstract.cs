702a1b333eed8297eef1e8db94e101641a59e4da
Devslops <alexisbrunet2000@live.Fr>
2021-03-02 15:11:21 +0100

    tp3 abstract

---
+++ tp1/ComputerAbstract.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp1
+{
+    public abstract class ComputerAbstract
+    {
+        public abstract void DisplayComputer();
+    }
+}
